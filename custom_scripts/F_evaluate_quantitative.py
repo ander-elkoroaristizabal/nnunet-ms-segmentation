a9ba8322cc330d45f3236bc048541cfc46e4bf77
ander <elkoro9@gmail.com>
2023-05-07 21:37:40 +0200

    [Upd] Better quantitative and qualitative evaluation

--- custom_scripts/F_evaluate_quantitative.py
+++ custom_scripts/F_evaluate_quantitative.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 
 import pandas as pd
 import torch
+from tqdm import tqdm
 
 import nibabel as nib
 import numpy as np
@@ -24,6 +25,7 @@ from custom_scripts.A_config import (
 )
 from nnunetv2.inference.predict_from_raw_data import predict_from_raw_data
 from nnunetv2.evaluation.evaluate_predictions import compute_metrics_on_folder_simple, load_summary_json
+from scipy.ndimage import label
 
 
 def get_paths(case_id: str, test_dataset: TestDataset, basal_im: bool = False, follow_up_im: bool = False,
@@ -84,11 +86,57 @@ def format_results_into_df(all_results: Dict):
 def get_all_preds_and_labels(ids: List[str], test_dataset: TestDataset):
     labels = dict()
     preds = dict()
-    for case in ids:
+    for case in tqdm(ids):
         sample_labels, sample_predictions = read_labels_and_preds(case_id=case, test_dataset=test_dataset)
         labels[case] = sample_labels
         preds[case] = sample_predictions
     return labels, preds
+
+
+def compute_lesion_level_metrics(labels, preds, lesion_class: int):
+    gt_lesions, n_gt_lesions = label((labels == lesion_class).astype(int))
+    predicted_lesions, n_predicted_lesions = label((preds == lesion_class).astype(int))
+    # Making lesion names disjoint:
+    new_predicted_lesions = np.copy(predicted_lesions)
+    for pred_lesion_id in range(1, n_predicted_lesions + 1):
+        # print(f"{pred_lesion_id = }")
+        new_pred_lesion_id = pred_lesion_id + n_gt_lesions
+        # print(f"{new_pred_lesion_id = }")
+        new_predicted_lesions[predicted_lesions == pred_lesion_id] = new_pred_lesion_id
+    tp_gt = set(np.unique(gt_lesions[(gt_lesions > 0) & (predicted_lesions > 0)]))
+    tp_pred = set(np.unique(new_predicted_lesions[(gt_lesions > 0) & (new_predicted_lesions > 0)]))
+    fp = set(np.unique(new_predicted_lesions[(gt_lesions == 0) & (new_predicted_lesions > 0)])) - tp_pred
+    fn = set(np.unique(gt_lesions[(gt_lesions > 0) & (new_predicted_lesions == 0)])) - tp_gt
+    tn = set(np.unique(gt_lesions[(gt_lesions == 0) & (new_predicted_lesions == 0)]))
+    tp = len(tp_gt)
+    fp = len(fp)
+    fn = len(fn)
+    assert len(tp_pred) + fp == n_predicted_lesions, f"{tp_pred = }, {fp = }, {n_predicted_lesions = }"
+    assert len(tp_gt) + fn == n_gt_lesions, f"{tp_gt = }, {fn = }, {n_gt_lesions = }"
+    return n_gt_lesions, n_predicted_lesions, tp, fp, fn
+
+
+def compute_all_lesion_level_metrics(ids, labels_dict, preds_dict, lesion_class=2):
+    lesion_level_metrics = []
+    lesion_str = "new" if lesion_class == 2 else "basal"
+    for case in tqdm(ids):
+        n_gt, n_pred, tp, fp, fn = compute_lesion_level_metrics(labels=labels_dict[case],
+                                                                preds=preds_dict[case],
+                                                                lesion_class=lesion_class)
+        try:
+            F1 = 2 * tp / (2 * tp + fp + fn)
+        except ZeroDivisionError:
+            F1 = np.NAN
+        lesion_level_metrics.append({
+            "case_id": case,
+            f"n_ref_{lesion_str}_lesions": n_gt,
+            f"n_pred_{lesion_str}_lesions": n_pred,
+            f"{lesion_str}_lesion_tp": tp,
+            f"{lesion_str}_lesion_fp": fp,
+            f"{lesion_str}_lesion_fn": fn,
+            f"{lesion_str}_lesion_F1": F1
+        })
+    return pd.DataFrame.from_records(data=lesion_level_metrics)
 
 
 if __name__ == '__main__':
@@ -129,25 +177,40 @@ if __name__ == '__main__':
     # TEST:
     test_results_dict = load_summary_json(TEST_PREDICTIONS_FOLDER + "/" + 'summary.json')
     test_results_df = format_results_into_df(test_results_dict)
+
+    # Getting labels and predictions:
+    test_images = os.listdir(TEST_IMAGES_DIR)
+    test_ids = sorted({file_name.split(".")[0][:-5] for file_name in test_images})
+    all_labels, all_preds = get_all_preds_and_labels(ids=test_ids, test_dataset=TestDataset.test_split)
+
+    # Adding lesion-wise results:
+    test_lesion_wise_res = compute_all_lesion_level_metrics(ids=test_ids, labels_dict=all_labels, preds_dict=all_preds)
+    test_results_df = test_results_df.merge(test_lesion_wise_res, on='case_id')
+
     # Results on no-lesions subset:
-    new_cols_mask = (test_results_df.columns == "case_id") | test_results_df.columns.str.startswith("new_")
+    new_cols_mask = (test_results_df.columns == "case_id") | test_results_df.columns.str.contains("new_")
     print(test_results_df.loc[test_results_df.new_n_ref == 0, new_cols_mask])
     # Results on with-lesions subset:
     print(test_results_df.loc[test_results_df.new_n_ref > 0, new_cols_mask])
+    del all_preds, all_labels  # Memory issues
 
     # MSSEG2:
     MSSEG2_results_dict = load_summary_json(MSSEG2_PREDICTIONS_DIR / 'summary.json')
     MSSEG2_results_df = format_results_into_df(MSSEG2_results_dict)
+
+    # Getting labels and predictions:
+    test_images = os.listdir(MSSEG2_LABELS_DIR)
+    test_ids = sorted({file_name[:-7] for file_name in test_images})
+    all_labels, all_preds = get_all_preds_and_labels(ids=test_ids, test_dataset=TestDataset.msseg2)
+
+    # Adding lesion-wise results:
+    lesion_wise_res = compute_all_lesion_level_metrics(ids=test_ids, labels_dict=all_labels, preds_dict=all_preds)
+    MSSEG2_results_df = MSSEG2_results_df.merge(lesion_wise_res, on='case_id')
+
     # Results on no-lesions subset:
     print(MSSEG2_results_df.loc[MSSEG2_results_df.new_n_ref == 0, :])
     # Results on with-lesions subset:
     print(MSSEG2_results_df.loc[MSSEG2_results_df.new_n_ref > 0, :])
-
-    # Getting labels and predictions:
-    test_images = os.listdir(MSSEG2_LABELS_DIR)
-    # test_ids = sorted({file_name.split(".")[0][:-5] for file_name in test_images})
-    test_ids = sorted({file_name[:-7] for file_name in test_images})
-    all_labels, all_preds = get_all_preds_and_labels(ids=test_ids, test_dataset=TestDataset.msseg2)
 
     # Confusion matrix:
     all_flattened_labels = np.concatenate([labels.flatten() for labels in all_labels.values()])
